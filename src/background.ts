27e15c5d27e581d5ca31ad171b7e30eccd1da43f
desmond123-arch <desmondopokumends2004@gmail.com>
2024-12-25 23:02:14 +0000

    added comments

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,4 @@
+//inject content scripts programmatically
 chrome.runtime.onInstalled.addListener(async () => {
     for (const cs of chrome.runtime.getManifest().content_scripts!) {
         for (const tab of await chrome.tabs.query({ url: cs.matches })) {
@@ -26,6 +27,7 @@ chrome.runtime.onInstalled.addListener(async () => {
 
 let mySelection: any;
 
+//show pop for pdfs only
 chrome.tabs.onActivated.addListener(function (info) {
     var tab = chrome.tabs.get(info.tabId, function (tab) {
         console.log(tab)
@@ -40,16 +42,17 @@ chrome.tabs.onActivated.addListener(function (info) {
     })
 });
 
-
+//when on normal website
 chrome.action.onClicked.addListener(async (tab) => {
     const response = await chrome.tabs.sendMessage(tab.id as number, { type: "getWord" });
     chrome.tabs.sendMessage(tab.id as number, { type: "definition", definition: response.selectedWord })
 })
 
+//message handlers
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    //
     if (request.action === "selectedText") {
         mySelection = request.text;
-        console.log(mySelection)
         chrome.storage.sync.set({mySelection})
     }
     if (request.action === "Handshake") {
@@ -57,6 +60,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     }
 })
 
+//pdf handler
 chrome.contextMenus.onClicked.addListener((info, tab) => {
     mySelection = info.selectionText;
     chrome.storage.sync.set({mySelection});
