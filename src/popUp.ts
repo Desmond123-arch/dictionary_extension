48e8c1a8a8a61389ca05db28146bc33957d46b11
desmond123-arch <desmondopokumends2004@gmail.com>
2024-12-27 00:41:29 +0000

    added support for normal btn

--- src/popUp.ts
+++ src/popUp.ts
@@ -1,6 +1,6 @@
 const button = document.querySelector("#define-btn");
-const wordInput = document.querySelector("#word-input");
-const selectedWord = document.querySelector("s#electedWord");
+const wordInput = document.querySelector("#word-input") as HTMLInputElement;
+const selectedWord = document.querySelector("#selectedWord");
 const meaning = document.querySelector("#meaning");
 let myWord = "";
 let selections;
@@ -12,7 +12,7 @@ async function defineWord(word: string | null | undefined): Promise<string[]> {
     }
     const explanations: string[] = [];
     const language = navigator.language;
-    console.log(language);
+    // console.log(language);
     try {
         const response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
         response.data[0].meanings[0].definitions.map((data: any) => {
@@ -31,14 +31,27 @@ function updatePopUp() {
     chrome.storage.sync.get(['mySelection'], async (data) => {
         wordInput?.setAttribute('value', data.mySelection);
         const explanations = await defineWord(wordInput?.getAttribute('value'));
-        console.log(explanations);
         if (meaning) {
-            meaning.innerHTML = explanations[0]
+            meaning.innerHTML = explanations[0] || ""
         }
 
+        if (selectedWord) {
+            selectedWord.innerHTML =  data.mySelection || "";
+        }
     })
 
 }
 
+button?.addEventListener("click", async() => {
+    const inputWord = wordInput?.value;
+    const explanations = await defineWord(inputWord)
+    if (meaning) {
+        meaning.innerHTML = explanations[0] || ""
+    }
+    console.log(selectedWord);
+    if (selectedWord) {
+        selectedWord.innerHTML =  explanations[0] || "";
+    }
+});
 
 document.addEventListener('DOMContentLoaded', updatePopUp);
